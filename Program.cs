36360a52cdcd11699053c77d601e74627cdc23f2
Prashant Singh <Prashantsrajput@gmail.com>
2026-09-06 21:32:25 +0530

    Master Branch - 1

--- Program.cs
+++ Program.cs
@@ -6,17 +6,7 @@ namespace Prashant_Singh
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("Line Number " + i);
-            }
-            Console.WriteLine("Kya ho rha hai ");
-            int i = 0;
-            do
-            {
-                Console.WriteLine("Hello I am from master branch" + i);
-                i++;
-            } while (i < 10);
+            Console.WriteLine("This is master branch");
         }
     }
 }
