39e72567da66f4d49b8f111092f6def9d92624c8
Prashant Singh <Prashantsrajput@gmail.com>
2026-09-06 15:43:37 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prashant_Singh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
